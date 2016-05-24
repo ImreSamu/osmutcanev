50d4869382ad3efdcead57bafa9faa77df78d57d
ImreSamu <ImreSamu@users.noreply.github.com>
2016-05-24 15:13:13 +0200

    correcting OSM Download scipts ( md5 check )

--- download_osmadmin.sh
+++ download_osmadmin.sh
@@ -6,6 +6,10 @@ set -o nounset
 readonly OSM_ADMIN_BASE=hungary-160101
 
 cd ./input/osmadmin
-rm -f *.osm.pbf
+rm -f *.pbf
+rm -f *.md5
 wget http://download.geofabrik.de/europe/${OSM_ADMIN_BASE}.osm.pbf
+wget http://download.geofabrik.de/europe/${OSM_ADMIN_BASE}.osm.pbf.md5
+
+md5sum -c *.md5
 
--- download_osmlatest.sh
+++ download_osmlatest.sh
@@ -3,20 +3,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-
 cd ./input/osmdata
 
 cd ../osmdata
-rm -f hungary-latest.osm.pbf
+rm -f *.pbf
+rm -f *.md5
 wget http://download.geofabrik.de/europe/hungary-latest.osm.pbf
+wget http://download.geofabrik.de/europe/hungary-latest.osm.pbf.md5
 
-
-
-
-
-
-
-
-
-
-
+md5sum -c *.md5
